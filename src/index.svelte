208ba2d26cd2f8ad5652efc066be5d32320cb0f9
A <foote.a@gmail.com>
2020-05-11 14:46:21 +1200

    added input and encrypting function

--- src/index.svelte
+++ src/index.svelte
@@ -1,3 +1,29 @@
-<h1>Svelte</h1>
+<script>
+let input = ''
+let output = ''
 
-<p>Welcome to coding with Svelte!</p>
+function encrypt() {
+    let plaintext = input
+    let ciphertext = ''
+
+    for(let i = 0; i < plaintext.length; i++) {
+        ciphertext += plaintext.charCodeAt(i) + ' '
+    }
+
+    output = ciphertext
+}
+</script>
+
+
+<h1>Cipher</h1>
+
+<label>
+Text
+<input type="text" bind:value={input}>
+</label>
+
+<button on:click={encrypt}>Encrypt</button>
+
+
+<h2>Result</h2>
+<p>{output}</p>
